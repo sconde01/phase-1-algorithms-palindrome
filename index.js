79c848e5ba2b08ae5929ed696e2739953176d047
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-05-06 21:27:06 -0700

    commit tree 69fce17881251b9912734d5543f11d0905b1588f

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+//----------------------------------------------writing it like this helped me undersand better and workd:
+// const isPalindrome =(word) => {
+//     let newString = "";
+//     for (let i = word.length - 1; i >= 0; i--){
+//       newString += word[i];
+//     }
+//     return newString === word;
+//   }  
+
+// isPalindrome ("racecar"); 
+// What did you do here, Stephanie? I created a new string for "word" breaking each letter down by number...then reversing each letter assigned by number...wheeeeww
+
+//------------------------Optimize:
+// const reverse = (word) => {
+//   return word.split("").reverse().join("");
+// }
+
+// const isPalindrome = (word) =>{
+//   const reversedWord = reverse(word);
+//   return word === reversedWord;
+// }
+//optimize further!:
+
+function reverse (word){
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome (word){
+  const reversedWord = reverse(word);
+  return word === reversedWord; 
 
-/*
-  Add written explanation of your solution here
-*/
+}
+
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
